1b943680961e6eecc0b38b76baa620372c278299
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-12-04 11:15:27 -0600

    Added Cors

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7285",
+                                              "http://localhost:3000")
+                                               .AllowAnyHeader()
+                                               .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
